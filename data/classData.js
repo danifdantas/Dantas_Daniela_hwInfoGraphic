b521fd6fb50d96754e43821422778dc3c11c4886
Dani Dantas <dani@DaniVonRohr.local>
2018-10-31 11:56:12 -0400

    calling data from a js file - started

---
+++ data/classData.js
@@ -0,0 +1,26 @@
+const classData = {
+  pi: [
+    "Chlotrudis Awards : Best Cinematography - Matthew Libatique ",
+    "Independent Spirit Award: Best First Screenplay - Darren Aronofsky ",
+    "Florida Film Critics Circle Awards: Newcomer of the Year: Darren Aronofsky ",
+    "Gotham Awards: Open Palm Award - Darren Aronofsky",
+    "Málaga International Week of Fantastic Cinema: Special Mention	For the screenplay: Darren Aronofsky",
+    "Sundance Film Festival: Directing Award - Darren Aronofsky"
+  ],
+
+  requiem: [
+    "Stockholm Film Festival: Best Actress - Ellen Burstyn ",
+    "Florida Film Critics Circle Awards: Best Actress - Ellen Burstyn",
+    "Academy Awards, USA: Oscar Nominee: Best Actress in a Leading Role - Ellen Burstyn",
+    "AFI Awards, USA: Movie of the Year",
+    "Boston Society of Film Critics Awards: Best Actress - Ellen Burstyn",
+    "Golden Globes, USA Nominee - Best Performance by an Actrees (Drama) - Ellen Burstyn"
+  ],
+
+  fountain: ["Award 1", "Award 2"],
+
+  blackSwan: ["Award 1", "Award 2"],
+  wrestler: ["Award 1", "Award 2"],
+
+  mother: ["Award 1", "Award 2"]
+};
