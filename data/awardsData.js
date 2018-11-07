e41a936675b82cfef268ac6b6013511be95b23da
Dani Dantas <dani@DaniVonRohr.local>
2018-11-06 19:41:48 -0500

    last changes

---
+++ data/awardsData.js
@@ -0,0 +1,54 @@
+const classData = {
+  pi: [
+    "Chlotrudis Awards : Best Cinematography - Matthew Libatique ",
+    "Independent Spirit Award: Best First Screenplay - Darren Aronofsky ",
+    "Florida Film Critics Circle Awards: Newcomer of the Year: Darren Aronofsky ",
+    "Gotham Awards: Open Palm Award - Darren Aronofsky",
+    "Málaga International Week of Fantastic Cinema: Special Mention	For the screenplay: Darren Aronofsky",
+    "Sundance Film Festival: Directing Award - Darren Aronofsky"
+  ],
+
+  requiem: [
+    "Stockholm Film Festival: Best Actress - Ellen Burstyn ",
+    "Florida Film Critics Circle Awards: Best Actress - Ellen Burstyn",
+    "Academy Awards, USA: Oscar Nominee: Best Actress in a Leading Role - Ellen Burstyn",
+    "AFI Awards, USA: Movie of the Year",
+    "Boston Society of Film Critics Awards: Best Actress - Ellen Burstyn",
+    "Golden Globes, USA Nominee - Best Performance by an Actrees (Drama) - Ellen Burstyn"
+  ],
+
+  fountain: [
+    "Golden Globes, USA Nominee - Best Original Score - Motion Picture",
+    "Chicago Film Critics Association Awards - Best Original Score - Clint Mansell",
+    "Golden Schmoes Awards - Most Underrated Movie of the Year",
+    "New York Film Critics, Online - Top Films of the Year",
+    "Online Film Critics Society Awards - Best Original Score Clint Mansell",
+    "Venice Film Festival - Nominee - Darren Aronofsky"
+  ],
+
+  blackSwan: [
+    "Academy Awards, USA - Best Performance by an Actress in a Leading Role Natalie Portman",
+    "BAFTA Awards - Best Leading Actress Natalie Portman",
+    "Academy of Motion Picture Arts and Sciences of Argentina - Best Foreign Film Darren Aronofsky USA.",
+    "Saturn Award - Best Actress Natalie Portman & Best Supporting Actress Mila Kunis",
+    "AFI Awards, USA - Movie of the Year",
+    "Alliance of Women Film Journalists - Best Film Editing Andrew Weisblum"
+  ],
+  wrestler: [
+    "Golden Globes, USA - Best Performance by an Actor in a Motion Picture - Drama Mickey Rourke",
+    "Golden Globes, USA - Best Original Song - Motion Picture Bruce Springsteen (music/lyrics) Song: The Wrestler",
+    "BAFTA Award - Best Leading Actor Mickey Rourke",
+    "Awards Circuit Community Awards - Best Actor in a Leading Role Mickey Rourke",
+    "Central Ohio Film Critics Association - Best Supporting Actress Marisa Tomei",
+    "Film Independent Spirit Awards - Best Feature Darren Aronofsky & Scott Franklin"
+  ],
+
+  mother: [
+    "Awards Circuit Community Awards - Honorable Mentions Darren Aronofsky",
+    "CinEuphoria Awards - Top Ten of the Year - International Competition Darren Aronofsky",
+    "Fright Meter Awards - Best Editing Andrew Weisblum",
+    "Golden Schmoes Awards - Trippiest Movie of the Year",
+    "Golden Trailer Awards - Best Sound Editing Paramount Pictures Buddha Jones",
+    "International Online Cinema Awards (INOCA) - Best Sound Mixing Craig Henighan, Noyan Cosarer, Simon Poudrette. Tied with Baby Driver (2017)."
+  ]
+};
